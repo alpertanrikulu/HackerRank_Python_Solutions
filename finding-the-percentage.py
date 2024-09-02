32c12b058ec6f981d4fc990d0067de65c5769074
Alper Tanrıkulu <tanrikulualper77100@gmail.com>
2024-09-02 21:34:26 +0300

    Create finding-the-percentage.py

---
+++ finding-the-percentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    a = student_marks[query_name]
+    a = sum(a)/3
+    print(f'{a:.2f}')
