9c2877a8bdd5c042a40997ac94d021d29604e294
Alper Tanrıkulu <tanrikulualper77100@gmail.com>
2024-09-19 10:17:39 +0300

    Create py-set-difference-operation.py

---
+++ py-set-difference-operation.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n1 = input()
+c1 = set(input().split())
+
+n2 = input()
+c2 = set(input().split())
+
+result = c1.difference(c2)
+print(len(result))
