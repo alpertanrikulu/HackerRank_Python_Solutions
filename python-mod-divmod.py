b84e5993fedd367583497e4a0989f8c6b56cb202
Alper Tanrıkulu <tanrikulualper77100@gmail.com>
2024-09-21 10:57:19 +0300

    Create python-mod-divmod.py

---
+++ python-mod-divmod.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+d = int(input())
+
+result = divmod(n, d)
+print(result[0])
+print(result[1])
+print(result)
